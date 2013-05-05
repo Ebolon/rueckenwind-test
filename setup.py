666165b437c527f0d411ca0fe96ea87596a0da6c
Simon Pizonka <simon.pizonka@gmail.com>
2013-05-05 11:23:46 +0200

    Added rueckenwind dependency.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "rueckenwind_test",
     version = "0.0.1",
+    install_requires=['rueckenwind'],
     packages = find_packages(),
 
 )
